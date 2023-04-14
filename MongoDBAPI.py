ab266ec2b8a60c9fbbe0a89e72f03b828aadea62
Amir Katorza <amir.katorza@gmail.com>
2023-04-14 12:22:19 +0300

    New app Architecture, OOP

---
+++ MongoDBAPI.py
@@ -0,0 +1,57 @@
+from pymongo import MongoClient
+import gridfs
+import logging as log
+import TMDB_Downloader
+
+
+class MongoAPI:
+    def __init__(self, db_name, collection, ip="localhost", port=27017):
+        self.client = MongoClient(host=ip,
+                                  port=port,
+                                  serverSelectionTimeoutMS=10
+                                  )
+        self.collection = collection
+        self.db = self.client[db_name]
+        self.fs = gridfs.GridFS(self.db, collection)
+
+    def write_image(self, file_name, movie_name, imdb_id, byte_arr):
+        log.info('Writing Data')
+        fs_id = self.fs.put(byte_arr, movie_name=movie_name, imdb_id=imdb_id, filename=file_name)
+        output = {'Status': 'Successfully Inserted',
+                  'Document_ID': str(fs_id)}
+        return output
+
+    def read_image(self, movie_name):
+        f_id = self.db.posters.files.find_one({"movie_name": movie_name}, {"_id": 1})
+        byte_arr = self.fs.get(f_id['_id']).read()
+        with open(movie_name + ".jpeg", 'wb') as w:
+            w.write(byte_arr)
+        return f_id
+
+    def get_file_id_by_name(self, movie_name):
+        return self.db.posters.files.find_one({"movie_name": movie_name}, {"_id": 1})
+
+    def del_image(self, movie_name):
+        log.info('Deleting Data')
+        f_id = self.get_file_id_by_name((movie_name))
+        self.fs.delete(f_id)
+        output = {'Status': 'Successfully Deleted' if f_id else "Nothing was Deleted."}
+        return output
+
+    def update_image_file_meta_data(self, movie_name, key_to_update, val_to_update):
+        f_id = self.get_file_id_by_name((movie_name))
+        mycol = self.db["posters.files"]
+        myquery = {"_id": f_id}
+        new_values = {"$set": {key_to_update: val_to_update}}
+        db_update_response = mycol.update_one(myquery, new_values)
+        output = {'Status': 'Successfully Updated' if db_update_response.modified_count > 0 else "Nothing was updated."}
+        return output
+
+
+if __name__ == '__main__':
+    mdb = MongoAPI("movies", "posters")
+    download_agent = TMDB_Downloader.TMDBDownloader()
+    moviename = "Harry Potter"
+    movieid, file_name, byte_arr = download_agent.download_poster(moviename)
+    mdb.write_image(file_name, moviename, movieid, byte_arr)
+    mdb.read_image(moviename)
--- TMDB_Downloader.py
+++ TMDB_Downloader.py
@@ -1,81 +1,47 @@
-from credentials import *
+from credentials import API_KEY_V3
 import requests
 import imdb
-import pymongo
-import gridfs
 
 
-def mongo_conn():
-    try:
-        conn = pymongo.MongoClient(
-            host="localhost",
-            port=27017,
-            serverSelectionTimeoutMS=10
-        )
-        conn.server_info()  # trigger exception if you cannot connect to db
-        print("MongoDB connected", conn)
-        return conn
-        return conn.grid_file
-    except Exception as e:
-        print("Error in mongo connection: ", e)
-
-
-def config_poster_download():
+class TMDBDownloader:
     CONFIG_PATTERN = 'http://api.themoviedb.org/3/configuration?api_key={key}'
-    url = CONFIG_PATTERN.format(key=API_KEY_V3)
-    result = requests.get(url)
-    config = result.json()
-    base_url = config["images"]["base_url"]
-    poster_size = config["images"]["poster_sizes"][3]
-    return base_url, poster_size
-
-
-def get_imdb_id(movie_name: str) -> dict:
-    ia = imdb.IMDb()
-    items = ia.search_movie(movie_name)
-    movie_dict = {}
-    for movie in items:
-        movie_id = movie.movieID  # Key
-        movie_name = f"{movie}"   # Value
-        movie_dict[movie_id] = movie_name
-    return movie_dict
-
-
-def get_posters_urls(movie_id):
     IMG_PATTERN = "http://api.themoviedb.org/3/movie/{imdbid}/images?api_key={key}"
-    result = requests.get(IMG_PATTERN.format(key=API_KEY_V3, imdbid=f"tt{movie_id}"))
-    api_response = result.json()
-    # json_formatted_str = json.dumps(api_response, indent=2)
-    # print(json_formatted_str)
-    poster_path = api_response["posters"][0]["file_path"]
-    return poster_path
 
+    def __init__(self):
+        url = self.CONFIG_PATTERN.format(key=API_KEY_V3)
+        result = requests.get(url)
+        config = result.json()
+        self.base_url = config["images"]["base_url"]
+        self.poster_size = config["images"]["poster_sizes"][3]
 
-def index_poster(movies_dict):
-    db = mongo_conn().movies
-    fs = gridfs.GridFS(db, "posters")
-    base_url, size = config_poster_download()
-    for movie_id, movie_name in movies_dict.items():
+    def _get_movie_id(self, movie_name: str):
+        ia = imdb.IMDb()
+        items = ia.search_movie(movie_name)
+        imdb_id = items[0].movieID
+        return imdb_id
+
+    def _get_poster_url(self, movie_id):
+        result = requests.get(self.IMG_PATTERN.format(key=API_KEY_V3, imdbid=f"tt{movie_id}"))
+        api_response = result.json()
+        poster_path = api_response["posters"][0]["file_path"]
+        return poster_path
+
+    def download_poster(self, movie_name):
+        movie_id = self._get_movie_id(movie_name)
         try:
-            poster_path = get_posters_urls(movie_id)
+            poster_path = self._get_poster_url(movie_id)
         except Exception as e:
             print(f"Could not find movie - {movie_name} - {movie_id}")
         else:
-            url = base_url + size + poster_path
+            url = self.base_url + self.poster_size + poster_path
             r = requests.get(url)
             filetype = r.headers['content-type'].split('/')[-1]
             filename = f"poster_{movie_id}.{filetype}"
-            fs.put(r.content, movie_name=movie_name, imdb_id=movie_id, filename=filename)
+            return movie_id, filename, r.content
 
 
-movie_search = input("Please enter movie name to search: ")
-search_results = get_imdb_id(movie_search)
-index_poster(search_results)
-
-# movies_db = mongo_conn().movies
-# query = movies_db.posters.files.find({'movie_name': {'$regex': 'Avatar'}})
-# # for item in query:
-# #     print(item)
-# print(list(query))
-# if len(list(query)) == 0:
-
+if __name__ == "__main__":
+    download_agent = TMDBDownloader()
+    moviename = "scream"
+    movieid, file_name, byte_arr = download_agent.download_poster(moviename)
+    print(movieid, file_name)
